27fa254d930b38d4a97efacc7e4c5edf1786d8df
Simon Oberhammer <simon@nekapuzer.at>
2012-08-10 14:29:47 +0200

    add a simple example application

---
+++ examples/main.js
@@ -0,0 +1,24 @@
+var {renderResponse} = require('reinhardt');
+
+var context = {
+   links: [
+      ['Main', '/'],
+      ['Discovery of Jazz', './discoveryofjazz.html'],
+      ['Youth & Early Life', './earlylife.html'],
+      ['World War II', './worldwar2.html']
+   ],
+   djangoImage: require('ringo/base64').encode(require('fs').read(module.resolve('./static/django-reinhardt.jpg'), {binary: true}))
+}
+
+exports.app = function(req) {
+   var path = req.pathInfo.split('/').slice(1)[0] || 'base.html';
+   return renderResponse(path, context);
+};
+
+if (require.main == module) {
+   var {Loader} = require('reinhardt/loaders/filesystem');
+   var templateLoader = new Loader([module.resolve('./templates/'), module.resolve('./templates2')]);
+   require('reinhardt').registerLoader(templateLoader);
+
+   require("ringo/httpserver").main(module.id);
+}

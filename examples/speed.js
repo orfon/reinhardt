aab485b2d93a9a68f911c2ed836ca01c81bfb3c6
Simon Oberhammer <simon@nekapuzer.at>
2013-07-12 20:41:32 +0200

    run speedtest a bit longer and cleanup

--- examples/speed.js
+++ examples/speed.js
@@ -7,15 +7,17 @@ console.log('Starting speed tests...');
 ///////////// array loop
 var tpl = new Template("{% for foo in bar %}{{ forloop.counter }} - {{ foo }}{% endfor %}");
 
-console.time('array loop');
-var i = 10000;
-var d = new Date();
+var x = 2000;
+
+console.time('array loop ' + x);
+var d = Date.now();
+var i = x;
 while (i) {
   i -= 1;
   tpl.render({ bar: [ 'bar', 'baz', 'bop' ]});
 }
-console.timeEnd('array loop');
-console.log("  ~ " + Math.round(1000000 / (new Date() - d)) + " renders per sec.");
+console.timeEnd('array loop ' + x);
+console.log("  ~ " + Math.round(x * 1000 / (Date.now() - d)) + " renders per sec.");
 
 ///////// includes
 var env = new Environment({
@@ -25,26 +27,26 @@ var env = new Environment({
 var array = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "abcdefghijklmnopqrstuvwxyz"];
 tpl = env.getTemplate('include_base.html')
 
-i = 1000;
-console.time('Render 1000 Includes Templates');
-d = new Date();
+i = x;
+console.time('Render ' + x + ' Includes Templates');
+d = Date.now();
 while (i) {
   i -= 1;
   tpl.render({ array: array, foo: "baz", "included": "included.html" });
 }
-console.timeEnd('Render 1000 Includes Templates');
-console.log("  ~ " + Math.round(1000000 / (new Date() - d)) + " renders per sec.");
+console.timeEnd('Render ' + x + ' Includes Templates');
+console.log("  ~ " + Math.round(x * 1000 / (Date.now() - d)) + " renders per sec.");
 
 
 ////////// extends
 
 tpl = env.getTemplate("extends_2.html");
-i = 1000;
-console.time('Render 1000 Extends Templates');
-d = new Date();
+i = x;
+console.time('Render ' + x + ' Extends Templates');
+d = Date.now();
 while (i) {
   i -= 1;
   tpl.render({ array: array, foo: "baz", "included": "included.html" });
 }
-console.timeEnd('Render 1000 Extends Templates');
-console.log("  ~ " + Math.round(1000000 / (new Date() - d)) + " renders per sec.");
+console.timeEnd('Render ' + x + ' Extends Templates');
+console.log("  ~ " + Math.round(x * 1000 / (Date.now() - d)) + " renders per sec.");

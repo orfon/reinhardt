5958a08f486281e6ddc0bc754bbc4b99ed8511b4
Simon Oberhammer <simon@nekapuzer.at>
2013-09-19 13:13:53 +0200

    if has been implemented a while ago

--- test/nodelist.js
+++ test/nodelist.js
@@ -12,15 +12,12 @@ exports.testFor = function() {
     assert.equal(vars.length, 1);
 }
 
-/*
-FIXME if not implemented
 exports.testIf = function() {
     var source =  '{% if x %}{{ a }}{% endif %}';
     var template = new Template(source);
     var vars = template.nodeList.getByType(VariableNode)
     assert.equal(vars.length, 1);
 }
-*/
 
 exports.testIfEqual = function() {
     var source = '{% ifequal x y %}{{ a }}{% endifequal %}';

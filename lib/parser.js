orfon/reinhardt

commit 25d4b0993ac97d27828cb2f436b7533bef403493

re-added parser noop-functions - these are implemented by DebugParser
see https://github.com/orfon/reinhardt/commit/dc5041687b2dd33fba9777858692b7d198a77a2c#r50471128

lib/parser.js
@@ -63,6 +63,13 @@ Parser.prototype.parse = function(parseUntil) {
             if (!token.contents) {
                 this.emptyVariable(token);
             }
+            let filterExpression;
+            try {
+                filterExpression = this.compileFilter(token.contents);
+            } catch (e if e instanceof TemplateSyntaxError) {
+                this.compileFilterError(token, e);
+                throw e;
+            }
             let node = this.createVariableNode(this.compileFilter(token.contents));
             this.extendNodeList(nodeList, node, token);
         } else if (token.tokenType === constants.TOKEN_BLOCK) {
@@ -76,11 +83,20 @@ Parser.prototype.parse = function(parseUntil) {
                 this.prependToken(token);
                 return nodeList;
             }
+            this.enterCommand(command, token);
             let compileFunc = this.tags[command];
             if (!compileFunc || !(typeof (compileFunc) == 'function')) {
                 this.invalidBlockTag(token, command);
             }
-            this.extendNodeList(nodeList, compileFunc(this, token), token);
+            let compiledResult;
+            try {
+                compiledResult = compileFunc(this, token);
+            } catch (e if e instanceof TemplateSyntaxError) {
+                this.compileFunctionError(token, e);
+                throw e;
+            }
+            this.extendNodeList(nodeList, compiledResult, token);
+            this.exitCommand();
         }
     }
     if (parseUntil.length > 0) {
@@ -161,3 +177,8 @@ Parser.prototype.createNodeList = function() {
 Parser.prototype.error = function(token, msg) {
     return new TemplateSyntaxError(msg);
 };
+
+Parser.prototype.enterCommand = () => {};
+Parser.prototype.exitCommand = () => {};
+Parser.prototype.compileFunctionError = () => {};
+Parser.prototype.compileFilterError = () => {};

commit 77f505a82d93f208b3012526e847fcb861c988b6

fixed regression introduced in 25d4b0993ac97d27828cb2f436b7533bef403493

lib/parser.js
@@ -70,7 +70,7 @@ Parser.prototype.parse = function(parseUntil) {
                 this.compileFilterError(token, e);
                 throw e;
             }
-            let node = this.createVariableNode(this.compileFilter(token.contents));
+            let node = this.createVariableNode(filterExpression);
             this.extendNodeList(nodeList, node, token);
         } else if (token.tokenType === constants.TOKEN_BLOCK) {
             let command;